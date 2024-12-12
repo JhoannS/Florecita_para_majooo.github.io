15aefc8c6352bbe3df528539dbdc3047066a0644
Jhoann Sebastian Zamudio <96028868+JhoannS@users.noreply.github.com>
2024-12-12 11:42:58 -0500

    Delete Florecita_para_Majitoo directory

--- Florecita_para_Majitoo/main.js
+++
@@ -1,7 +0,0 @@
-alert('Me tienes encantao´con tu sonrisa jaja')
-onload = () => {
-  const c = setTimeout(() => {
-    document.body.classList.remove("not-loaded");
-    clearTimeout(c);
-  }, 1000);
-};
